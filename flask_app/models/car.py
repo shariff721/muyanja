439a8a7df10f9841f1957b30111dac40e0bc7ee5
sharif721 <smuyanja11@gmail.com>
2023-05-23 10:34:29 -0700

    final fix

--- flask_app/models/car.py
+++ flask_app/models/car.py
@@ -7,7 +7,7 @@ from flask import flash
 class Car:
     DB = "sharif_examDB"
 
-    def __init__(self,data):
+    def __init__(self, data):
         self.id = data["id"]
         self.price = data["price"]
         self.model = data["model"]
@@ -17,16 +17,15 @@ class Car:
 
         self.owner = None
 
-
     @classmethod
-    def add_car(cls,data):
+    def add_car(cls, data):
         query = """
             INSERT INTO cars (price, model, make, year, description, user_id)
             VALUES (%(price)s, %(model)s, %(make)s, %(year)s, %(description)s, %(user_id)s);
         """
-        results = connectToMySQL(cls.DB).query_db(query,data)
+        results = connectToMySQL(cls.DB).query_db(query, data)
         return results
-    
+
     @classmethod
     def get_all_cars_with_owner(cls):
         query = """
@@ -53,64 +52,62 @@ class Car:
             all_cars.append(one_car)
 
         return all_cars
-    
+
     @classmethod
-    def update_car(cls,data):
+    def update_car(cls, data):
         query = """
             UPDATE cars
             SET price = %(price)s, model = %(model)s, make = %(make)s, year = %(year)s, description = %(description)s
             WHERE id = %(id)s;
         """
-        return connectToMySQL(cls.DB).query_db(query,data)
-    
+        return connectToMySQL(cls.DB).query_db(query, data)
 
     @classmethod
-    def get_one_with_owner(cls,data):
+    def get_one_with_owner(cls, data):
         query = """
             SELECT * FROM cars JOIN users ON cars.user_id = users.id
             WHERE cars.id = %(id)s
         """
-        results = connectToMySQL(cls.DB).query_db(query,data)
+        results = connectToMySQL(cls.DB).query_db(query, data)
 
-        one_car = cls(results[0]) 
-        user_data = { 
-            'id':results[0]['users.id'],
-            'first_name':results[0]['first_name'],
-            'last_name':results[0]['last_name'],
-            'email':results[0]['email'],
-            'password':results[0]['password'],
-            'created_at':results[0]['users.created_at'],
-            'updated_at':results[0]['users.updated_at']
+        one_car = cls(results[0])
+        user_data = {
+            'id': results[0]['users.id'],
+            'first_name': results[0]['first_name'],
+            'last_name': results[0]['last_name'],
+            'email': results[0]['email'],
+            'password': results[0]['password'],
+            'created_at': results[0]['users.created_at'],
+            'updated_at': results[0]['users.updated_at']
         }
         # we create an instance of the User class with the user data.
         one_user = user.User(user_data)
-        one_car.owner = one_user  
+        one_car.owner = one_user
 
         return one_car
-    
+
     @classmethod
     def delete_car(cls, data):
         query = """
             DELETE FROM cars WHERE id = %(id)s;
         """
-        return connectToMySQL(cls.DB).query_db(query,data)
-    
+        return connectToMySQL(cls.DB).query_db(query, data)
+
     @classmethod
-    def add_buy(cls,data):
+    def add_buy(cls, data):
         query = """
             INSERT INTO buys
             (user_id, car_id) VALUES (%(user_id)s, %(car_id)s);
         """
-        return connectToMySQL(cls.DB).query_db(query,data)
+        return connectToMySQL(cls.DB).query_db(query, data)
 
-    
     @staticmethod
     def validate_cars(car):
         is_valid = True
 
         if len(car['price']) < 1:
             flash("price must not be blank.")
-            is_valid = False        
+            is_valid = False
 
         elif int(car['price']) <= 0:
             flash("invalid price.")
@@ -137,6 +134,16 @@ class Car:
             is_valid = False
 
         return is_valid
-    
-    
 
+    def is_sold(self):
+        query = """
+            SELECT * FROM buys WHERE car_id = %(car_id)s;
+        """
+        data = {
+            "car_id": self.id
+        }
+        results = connectToMySQL(Car.DB).query_db(query, data)
+        if len(results) < 1:
+            return False
+        else:
+            return True
